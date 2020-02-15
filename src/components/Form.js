eb4a056750674dc2c2cbd0e2d554fa254559b0c4
ebisLab <ebaiden@techie.com>
2020-02-15 15:47:13 -0500

    created form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const Form = () =>{
-    return(<div>
+    return(<div style={{width: '200px', display: 'inline-block'}}>
         <h2>Hello World</h2>
+        <label>Title</label>
+        {/* <input/> */}
+        <input placeholder="Write your title"/>
+        <textarea rows="4" cols="50" name="comment" form="usrform" placeholder="Write your blog" />
+        <button>Submit</button>
     </div>)
 }
 
