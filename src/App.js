2511ecfc08021c05de311d87a81733a4f107bdeb
ebisLab <ebaiden@techie.com>
2020-02-15 16:25:18 -0500

    created data and mapped through data contents

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form'
 
+const blogPost= [{
+   id: 1234566789,
+  title: 'La Cucaracia',
+  content: 'This song was from my early years'
+}]
+
+const Posts= (props) =>{
+  console.log('props in Posts', props)
+
+  return(<div>
+    <p>Title: {props.item.title}</p>
+    <p>Content: {props.item.content} </p>
+  </div>)
+}
+
 function App() {
+
+const [data, setData] = useState(blogPost)
+
   return (
     <div className="App">
-      <Form />
+      <Form data={data}/>
+      {data.map(item => (<Posts key={item.id} item={item} /> ))}
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Form = () =>{
+const Form = (props) =>{
+
+    const [info, setInfo] = useState([{
+        title: '',
+        contnet: ''
+    }])
+
+    const changeHandler = e =>{
+        setInfo(...info, e.target.value)
+    }
+
+    const handleSubmit= e =>{
+e.preventDefault();
+
+    }
+
+console.log(props, '<-props')
+
     return(<div style={{width: '200px', display: 'inline-block'}}>
         <h2>Hello World</h2>
+        <form onSubmit={handleSubmit}> 
         <label>Title</label>
-        {/* <input/> */}
         <input placeholder="Write your title"/>
-        <textarea rows="4" cols="50" name="comment" form="usrform" placeholder="Write your blog" />
+        <textarea rows="4" cols="50" name="comment" form="usrform" placeholder="Write your blog" onChange={changeHandler}/>
         <button>Submit</button>
+        </form>
+
+        {}
     </div>)
 }
 
